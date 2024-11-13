9ee6ec9158b9572b54c834fb78c98aeeb29a4a5f
Saro <sarobaridoo@gmail.com>
2024-11-14 00:17:41 +0100

    linked the server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose');
 
 //creating the dtatbase connection
-mongoose.connect()
+mongoose.connect(mongodb+srv://sarobaridoo:<db_password>@cluster0.cpwvy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
 
 const app = express();
 const PORT = process.env.PORT || 5000;
