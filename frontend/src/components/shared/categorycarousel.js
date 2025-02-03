fdd8fe1e6d9158476b53a9b202a968fb38e9fd0f
Saro <sarobaridoo@gmail.com>
2025-02-03 13:55:49 +0100

    Update categorycarousel.js

--- frontend/src/components/shared/categorycarousel.js
+++ frontend/src/components/shared/categorycarousel.js
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
+import "./style.css";
 
 const categories = [
   { name: "Development & IT", rating: "4.85/5", skills: "1853 skills" },
@@ -14,8 +15,9 @@ const categories = [
 
 const CategoryCarousel = () => {
   return (
-    <div className="w-full max-w-xl mx-auto my-20">
-      <h2 className="text-xl font-bold text-center mb-6">Browse talent by category</h2>
+    <div className="categorySec">
+      <h2 className="cat-heading">Browse talent by category</h2>
+<p className="carousel-body">Need a job? <span className="carousel-body-span"Browse our list</span></p>
       <Swiper
         modules={[Navigation]}
         spaceBetween={30}
@@ -25,10 +27,10 @@ const CategoryCarousel = () => {
       >
         {categories.map((cat, index) => (
           <SwiperSlide key={index}>
-            <div className="p-6 border rounded-lg shadow-md text-center bg-white">
-              <h3 className="text-lg font-semibold">{cat.name}</h3>
-              <p className="text-sm text-gray-600">⭐ {cat.rating}</p>
-              <p className="text-sm text-gray-500">{cat.skills}</p>
+            <div className="catReviews">
+              <h3 className="catReview">{cat.name}</h3>
+              <p className="catReview">⭐ {cat.rating}</p>
+              <p className="catReview">{cat.skills}</p>
             </div>
           </SwiperSlide>
         ))}
