9cb82248355274a7d4822fb255636a82eae58cf9
Saro <sarobaridoo@gmail.com>
2025-02-03 14:40:25 +0100

    job cards

---
+++ frontend/src/components/shared/latestjobcards.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Jobcards = () => {
+return(
+<div>
+<h1>Company Name</h1>
+<p>Country</p>
+</div>
+<div>
+<h1>Job Title</h1>
+<p>Job Description</p>
+</div>
+<div>
+<p className="badge O">Open position</p>
+<p className="badge P">Part time</p>
+<p className="badge R">Remote</p>
+</div>
+)
+}
+
+export default JobCards;
